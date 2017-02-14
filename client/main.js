3d79383bdbe0b14029e97141b835f5a0d47c8f37
Mohammad Hamid <mohammad200h@hotmail.com>
2017-02-15 00:31:12 +0400

    Second commit

--- client/main.js
+++ client/main.js
@@ -1,19 +1,16 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import { Parties } from '../collections/parties';
 
-angular.module('socially', [angularMeteor])
-.controller('PartiesListCtrl',PartiesListCtrl);
+angular.module('socially', [
+    angularMeteor
+  ])
+  .controller('PartiesListCtrl', function($scope) {
+    'ngInject';
 
-PartiesListCtrl.$inject = ['$scope'];
-function PartiesListCtrl($scope){
-      $scope.parties = [{
-        'name': 'Dubstep-Free Zone',
-        'description': 'Can we please just for an evening not listen to dubstep.'
-        }, {
-        'name': 'All dubstep all the time',
-        'description': 'Get it on!'
-        }, {
-        'name': 'Savage lounging',
-        'description': 'Leisure suit required. And only fiercest manners.'
-      }];
-};
+    $scope.helpers({
+      parties() {
+        return Parties.find({});
+      }
+    });
+  });
