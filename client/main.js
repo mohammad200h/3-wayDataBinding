18e71b8d8f8b43f92df7ffe18ca538d221f3f02d
Mohammad Hamid <mohammad200h@hotmail.com>
2017-02-15 01:29:01 +0400

    fifth commit not working

--- client/main.js
+++ client/main.js
@@ -1,22 +1,5 @@
-import angular from 'angular';
+
 import angularMeteor from 'angular-meteor';
 import { Parties } from '../collections/parties';
 
-angular.module('socially', [
-    angularMeteor
-  ])
-  .component('partiesList', {
-    templateUrl: 'client/partiesList.html',
-    controllerAs: 'partiesList',
-    controller($scope, $reactive) {
-      'ngInject';
-
-      $reactive(this).attach($scope);
-
-      this.helpers({
-        parties() {
-          return Parties.find({});
-        }
-      });
-    }
-  });
+import { name as Socially } from '../imports/ui/components/socially/socially';
